99d87449069debbcd6ec93c99a0401884e04232d
melcsk <mcheusk@umich.edu>
2025-04-19 19:01:16 -0400

    Completed Section 3 but skipped Date section until material is covered next week.

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -92,16 +92,26 @@ ORDER BY vendor_name, market_date;
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
-
-
+SELECT vendor_id
+, COUNT(vendor_id) AS booth_rental_times
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
+SELECT c.customer_last_name
+, c.customer_first_name
+, cp.customer_id
+, ROUND(SUM(cp.quantity*cp.cost_to_customer_per_qty), 2) AS total_spent
+FROM customer_purchases cp
 
-
+INNER JOIN customer c
+	ON cp.customer_id = c.customer_id
+GROUP BY customer_last_name, customer_first_name
+HAVING SUM(cp.quantity*cp.cost_to_customer_per_qty) > 2000;
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
@@ -114,6 +124,17 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+DROP TABLE IF EXISTS temp.new_vendor;
+
+CREATE TABLE temp.new_vendor(vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name);
+SELECT *
+FROM vendor;
+INSERT INTO temp.new_vendor(vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
+SELECT *
+FROM temp.new_vendor;
+
+-- I was not sure if the final answer is supposed to return both the list of vendors (vendor_id 1 to 9) in addition to a new row for vendor_id 10 or only new row for vendor_id 10?
 
 
 
